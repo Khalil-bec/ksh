3643f4de3abdaeaab95faf201c664f0a21cc86e6
Khalil-bec <khalil.becheikh54@gmail.com>
2026-03-16 00:39:54 +0100

    definir la fonction d'execution

---
+++ executor.py
@@ -0,0 +1,20 @@
+from my_builtins import BUILTINS
+import subprocess
+import inspect
+
+
+def execute_commande(cmd, args) :
+    #si la commande est une commande interne on l'exécute
+    if cmd in BUILTINS:
+        #verifier si la fonction prend des arguments ou pas
+        func = BUILTINS[cmd]
+        if len(inspect.signature(func).parameters) == 0:
+            func()
+        else:
+            func(args)
+    else :
+        #sinon on exécute la commande externe
+        try :
+            subprocess.run([cmd] + args)
+        except FileNotFoundError :
+            print(f"{cmd} : commande introuvable")
