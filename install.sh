faa06fd0e71db9b360e9c65b05886c07809b5a39
Khalil-bec <khalil.becheikh54@gmail.com>
2026-04-05 22:27:30 +0200

    ajout l'installation des dependences

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ set -e
 
 INSTALL_DIR="$(cd "$(dirname "$0")" && pwd)"
 LAUNCHER="/usr/local/bin/ksh"
+REQ_FILE="$INSTALL_DIR/requirements.txt"
 
 echo "Installation de ksh..."
 
@@ -12,8 +13,14 @@ if ! command -v python3 &>/dev/null; then
     exit 1
 fi
 
-# pyfiglet
-#pip3 install pyfiglet --quiet
+#installation des dépendances
+if [ -f "$REQ_FILE" ]; then
+    pip3 install --user -r "$REQ_FILE" --break-system-packages --quiet 2>/dev/null || \
+    pip3 install --user -r "$REQ_FILE" --quiet
+else
+    echo "Fichier requirements.txt introuvable, impossible d'installer les dépendances."
+    exit 1
+fi
 
 # Fichier alias
 [ ! -f "$HOME/.ksh_aliases" ] && echo "{}" > "$HOME/.ksh_aliases"
