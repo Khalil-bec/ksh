45cdbf2c62a1b4a3a75dc20ff02e49bc5ee4c08e
Khalil-bec <khalil.becheikh54@gmail.com>
2026-04-05 20:22:39 +0200

    implentation de autocomplétion

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import subprocess
 from welcome import afficher_bienvenue
 from parser import parse_command
 from executor import execute_commande
+from completer import configurer_autocomplete
+
+
 
 
 
 
 afficher_bienvenue()
-
+configurer_autocomplete()
 while True :
     try :
         line = input("Kshell> ")
